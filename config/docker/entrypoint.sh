8c4aedb54c68c941a8cdccdd90bfa7ee7391e9e5
Alan Zhu <2025azhu@tjhsst.edu>
2023-04-06 20:40:06 -0400

    fix(dev): fix buffer holding output until end of intial setup
    Python buffer holds output from initial
    setup script until everything is finished.
    Adding the -u flag makes Python run
    unbuffered.
    
    Add colors to text to make it easier to see.
    
    Run commands in the background when possible
    for speed.

--- config/docker/entrypoint.sh
+++ config/docker/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cp -u config/docker/secret.py intranet/settings
 
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+echo "Performing pre-startup tasks..."
+python3 -u config/docker/entrypoint.py &  # For initial setup
+sass --watch intranet/static/css:intranet/collected_static/css &  # Automatically compile modified scss files
 
 python3 manage.py runserver 0.0.0.0:8080
--- config/docker/initial_setup.sh
+++ config/docker/initial_setup.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
+echo "Copying secret.py to settings..."
+cp -u config/docker/secret.py intranet/settings
+
+echo "Copying create-users.py script..."
+cp config/create-users.py .
+
+echo "Collecting static files..."
+python3 -u manage.py collectstatic --noinput &
+
+echo "Running migrations..."
+python3 -u manage.py migrate &
+
 echo "Generating development data..."
 
 echo "Copying create user script..."
 cp config/create-users.py .
 
 echo "Creating development users..."
-python3 create-users.py -a admin
-python3 create-users.py -s student student1 student2 student3 2030student 2029student 2028student 2027student 2026student 2025student 2024student 2023student
-python3 create-users.py -t teacher teacher1 teacher2 teacher3
+python3 -u create-users.py -a admin &
+python3 -u create-users.py -s student student1 student2 student3 &
+python3 -u create-users.py -t teacher teacher1 teacher2 teacher3 &
+echo
 
 echo "Creating imported users..."
-python3 manage.py import_users config/user_import.json
-
-echo "Creating eighth period blocks..."
-python3 manage.py dev_create_blocks 12/31/2030
+python3 -u manage.py import_users config/data/outputs/user_import.json &
+echo
 
 echo "Creating eighth period activities..."
-python3 manage.py import_eighth config/eighth_import.json
+python3 -u manage.py import_eighth config/data/outputs/eighth_import.json &
+echo
 
-echo "Data generation complete."
+echo "Creating CSL apps..."
+python3 -u manage.py dev_create_cslapps &
+echo
